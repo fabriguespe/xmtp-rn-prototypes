fce99c6231f5346121e8ff5157cd70fe72c4d32f
fabri <fguespe@gmail.com>
2024-01-09 10:33:03 -0300

    consent

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-
-import {FloatingInbox} from './src/components/FloatingInbox-text/index.js';
-import {MessageContainer} from './src/components/FloatingInbox-text/MessageContainer';
+import React, {useState} from 'react';
+import {SafeAreaView, Button} from 'react-native';
+import {FloatingInbox as Floating1} from './src/components/FloatingInbox-text/index.js';
+import {FloatingInbox as Floating2} from './src/components/FloatingInbox-consent/index.js';
 import {XmtpProvider} from '@xmtp/react-native-sdk';
+
 function App() {
+  const [view, setView] = useState(null);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <XmtpProvider>
-        <FloatingInbox />
+        {!view && (
+          <>
+            <Button
+              title="Inbox Text"
+              onPress={() => setView('inbox1' as any)}
+            />
+            <Button
+              title="Inbox Consent"
+              onPress={() => setView('inbox2' as any)}
+            />
+            <Button
+              title="Inbox Consent"
+              onPress={() => setView('inbox3' as any)}
+            />
+          </>
+        )}
+        {view === 'inbox1' && <Floating1 />}
+        {view === 'inbox2' && <Floating2 />}
+        {view === 'inbox3' && <FloatingInbox />}
       </XmtpProvider>
     </SafeAreaView>
   );
